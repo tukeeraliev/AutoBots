cd36886720354ab1bd644e32e70e8a35c3bb370a
Tuke <tukeeraliev@gmail.com>
2025-07-16 14:28:22 -0400

    Visa card

---
+++ src/main/java/com/autobots/java/payment/VisaPayment.java
@@ -0,0 +1,8 @@
+package com.autobots.java.payment;
+
+public class VisaPayment {
+
+    public void payByVisaCard(double amount){
+        System.out.println("Visa card payment" + amount);
+    }
+}

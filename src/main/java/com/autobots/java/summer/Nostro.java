8809b8d66761640462068c4480704366e145fe18
Tuke <tukeeraliev@gmail.com>
2025-07-16 14:39:31 -0400

    Fixed

---
+++ src/main/java/com/autobots/java/summer/Nostro.java
@@ -0,0 +1,7 @@
+package com.autobots.java.summer;
+
+public class Nostro {
+    public static void main(String[] args) {
+        System.out.println("Nesto payment");
+    }
+}
---
+++ src/main/java/com/autobots/java/week_days/Search.java
@@ -0,0 +1,8 @@
+package com.autobots.java.week_days;
+
+public class Search {
+
+    public void searchData(String data){
+        System.out.println("Searching data: " + data);
+    }
+}
